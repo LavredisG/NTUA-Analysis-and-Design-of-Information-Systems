2cb0f33c99fe5bb47c287f6e805325a97eb74024
LavredisG <90918919+LavredisG@users.noreply.github.com>
2024-01-21 17:52:44 +0200

    Delete generate_data.sh

--- generate_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-02-12T00:00:00Z" --log-interval="10s" --format="influx" | gzip > ./datasets/influx_big.gz
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-02-12T00:00:00Z" --log-interval="10s" --format="timescaledb" | gzip > ./datasets/timescale_big.gz
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-01-10T08:00:00Z" --log-interval="10s" --format="influx" | gzip > ./datasets/influx_medium.gz
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-01-10T08:00:00Z" --log-interval="10s" --format="timescaledb" | gzip > ./datasets/timescale_medium.gz
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-01-02T13:45:00Z" --log-interval="10s" --format="influx" | gzip > ./datasets/influx_small.gz
-tsbs_generate_data --use-case="devops" --seed=123 --scale=10 --timestamp-start="2016-01-01T00:00:00Z" --timestamp-end="2016-01-02T13:45:00Z" --log-interval="10s" --format="timescaledb" | gzip > ./datasets/timescale_small.gz
